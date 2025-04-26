9f67e4588bed948c894a07c53554cc50a42910d7
Mattias Lindvall <mattias.lindvall@gmail.com>
2025-04-26 15:26:28 +0200

    Add vite preprocess to svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,13 @@
 import adapter from "@sveltejs/adapter-static";
 import { mdsvex } from "mdsvex";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 export default {
 	extensions: [".svelte", ".md"],
 
 	preprocess: [
+		vitePreprocess(),
+
 		mdsvex({
 			extensions: [".md"],
 		}),
